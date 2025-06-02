40ac7f5319405eb6592f9af3c6bb0ec1cc8d5b1c
Sirawit Chindam <Sirawithon@gmail.com>
2025-06-02 11:43:05 +0700

    lastest2

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,9 +1,12 @@
+
+
 import AllBook from '@/components/AllBook' ;
 import  getMe  from '@/lib/getMe' ;
 import { isMee } from '../../../interface';
 import Card from '@/components/Card';
 
 export default async function Home  () {
+  
   const isMe : isMee =await  getMe() ;
   
   return (
@@ -17,4 +20,45 @@ export default async function Home  () {
     </div>
   )
 };
+
+
+
+
+
+
+
+
+// "use client"
+// import AllBook from '@/components/AllBook' ;
+// import  getMe  from '@/lib/getMe' ;
+// import { isMee } from '../../../interface';
+// import Card from '@/components/Card';
+// import { useEffect } from 'react';
+// import { useState } from 'react';
+
+// export default function Home  () {
+//   const [Mee, setMe] = useState<isMee>();
+
+//   useEffect( () => {
+//     const fetchDataa = async () => {
+//     const isMe = await getMe();
+//     setMe(isMe) ;
+//   }
+//     fetchDataa()
+    
+//   },[])
+
+//   return (
+//   <div>
+//     {Mee === undefined ? (
+//       <p>Loading...</p>
+//     ) : Mee.role === "admin" ? (
+//       <AllBook />
+//     ) : (
+//       <Card />
+//     )}
+//   </div>
+// );
+
+// }
 
