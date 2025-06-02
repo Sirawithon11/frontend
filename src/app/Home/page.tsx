b6776c9e86f6e0c511e987c2fd7663f7009caafb
Sirawit Chindam <Sirawithon@gmail.com>
2025-06-02 11:43:20 +0700

    lastest3

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,64 +1,35 @@
 
-
+"use client"
 import AllBook from '@/components/AllBook' ;
 import  getMe  from '@/lib/getMe' ;
 import { isMee } from '../../../interface';
 import Card from '@/components/Card';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-export default async function Home  () {
-  
-  const isMe : isMee =await  getMe() ;
-  
+export default function Home  () {
+  const [Mee, setMe] = useState<isMee>();
+
+  useEffect( () => {
+    const fetchDataa = async () => {
+    const isMe = await getMe();
+    setMe(isMe) ;
+  }
+    fetchDataa()
+    
+  },[])
+
   return (
-    
-    <div>
-      {
-        isMe.role == "admin" ? 
-          <AllBook/>:<Card/>
-      }
-        
-    </div>
-  )
-};
+  <div>
+    {Mee === undefined ? (
+      <p>Loading...</p>
+    ) : Mee.role === "admin" ? (
+      <AllBook />
+    ) : (
+      <Card />
+    )}
+  </div>
+);
 
-
-
-
-
-
-
-
-// "use client"
-// import AllBook from '@/components/AllBook' ;
-// import  getMe  from '@/lib/getMe' ;
-// import { isMee } from '../../../interface';
-// import Card from '@/components/Card';
-// import { useEffect } from 'react';
-// import { useState } from 'react';
-
-// export default function Home  () {
-//   const [Mee, setMe] = useState<isMee>();
-
-//   useEffect( () => {
-//     const fetchDataa = async () => {
-//     const isMe = await getMe();
-//     setMe(isMe) ;
-//   }
-//     fetchDataa()
-    
-//   },[])
-
-//   return (
-//   <div>
-//     {Mee === undefined ? (
-//       <p>Loading...</p>
-//     ) : Mee.role === "admin" ? (
-//       <AllBook />
-//     ) : (
-//       <Card />
-//     )}
-//   </div>
-// );
-
-// }
+}
 
